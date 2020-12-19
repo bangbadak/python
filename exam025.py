860c10beb2d54881e167754e9b8b6e8cbc78891d
bangbadak <76235889+bangbadak@users.noreply.github.com>
2020-12-19 17:05:03 +0900

    Add files via upload

---
+++ exam025.py
@@ -0,0 +1,3 @@
+def calculate_area(radius):
+	area = 3.14 * radius**2
+	return area
---
+++ exam029.py
@@ -0,0 +1,8 @@
+def calculate_area(radius):
+	area = 3.14 * radius**2
+	return
+
+area = 0
+r = float(input("원의 반지름: "))
+calculate_area(r)
+print(area)
